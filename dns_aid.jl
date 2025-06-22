05ccf4398f9ebfb3475a0ba5cbf78c3f2ccf86b3
Syver Døving Agdestein <syverda@gmail.com>
2025-06-22 13:13:18 +0200

    Up

--- dns_aid.jl
+++ dns_aid.jl
@@ -20,11 +20,11 @@ using WGLMakie
 @info "Loading case"
 flush(stderr)
 
-# begin
-#     case = NavierStokes.smallcase()
-#     n_les = 50
-#     compression = 3
-# end
+begin
+    case = NavierStokes.smallcase()
+    n_les = 50
+    compression = 3
+end
 
 # begin
 #     case = NavierStokes.largecase()
@@ -32,11 +32,11 @@ flush(stderr)
 #     compression = 5
 # end
 
-begin
-    case = NavierStokes.snelliuscase()
-    n_les = 160
-    compression = 5
-end
+# begin
+#     case = NavierStokes.snelliuscase()
+#     n_les = 160
+#     compression = 5
+# end
 
 (; viscosity, outdir, datadir, plotdir, seed) = case
 g_dns = case.grid
@@ -56,29 +56,29 @@ end
 @info "Running experiment"
 flush(stderr)
 
-# experiment = "volavg"
-# sols, relerr = NavierStokes.dns_aid_volavg(;
-#     ustart,
-#     g_dns,
-#     g_les,
-#     poisson_dns,
-#     poisson_les,
-#     viscosity,
-#     compression,
-#     doproject = false,
-# );
+experiment = "volavg"
+sols, relerr = NavierStokes.dns_aid_volavg(;
+    ustart,
+    g_dns,
+    g_les,
+    poisson_dns,
+    poisson_les,
+    viscosity,
+    compression,
+    doproject = false,
+);
 
-# experiment = "project_volavg"
-# sols, relerr = NavierStokes.dns_aid_volavg(;
-#     ustart,
-#     g_dns,
-#     g_les,
-#     poisson_dns,
-#     poisson_les,
-#     viscosity,
-#     compression,
-#     doproject = true,
-# );
+experiment = "project_volavg"
+sols, relerr = NavierStokes.dns_aid_volavg(;
+    ustart,
+    g_dns,
+    g_les,
+    poisson_dns,
+    poisson_les,
+    viscosity,
+    compression,
+    doproject = true,
+);
 
 experiment = "surfavg"
 sols, relerr = NavierStokes.dns_aid_surface(;
@@ -99,26 +99,6 @@ flush(stderr)
 
 jldsave(joinpath(datadir, "$(experiment)_relerr.jld2"); relerr)
 
-# (;
-#     nomodel = norm(sols.nomodel.data - sols.dns_fil.data) / norm(sols.dns_fil.data),
-#     classic = norm(sols.classic.data - sols.dns_fil.data) / norm(sols.dns_fil.data),
-#     swapfil_symm = norm(sols.swapfil_symm.data - sols.dns_fil.data) /
-#                    norm(sols.dns_fil.data),
-#     swapfil = norm(sols.swapfil.data - sols.dns_fil.data) / norm(sols.dns_fil.data),
-# ) |> pairs
-
-# relerr |> pairs
-
-# let
-#     println(join(string.(keys(relerr)), " "))
-#     join(map(relerr) do v
-#         @sprintf "\$%.3g\$" v[1]
-#     end, " & ") |> println
-#     join(map(relerr) do v
-#         @sprintf "\$%.3g\$" v[end]
-#     end, " & ") |> println
-# end
-
 # Compute spectra
 let
     diss = ScalarField(g_dns)
@@ -133,81 +113,134 @@ let
 end
 
 let
-    fig = Figure()
-    ax = Axis(fig[1, 1]; xlabel = "Time")
-    lines!(ax, relerr.time, relerr.nomodel; label = "No-model")
-    lines!(ax, relerr.time, relerr.classic; label = "Classic")
-    lines!(ax, relerr.time, relerr.swapfil_symm; label = "Swap-sym")
-    lines!(ax, relerr.time, relerr.swapfil; label = "Swap")
-    # axislegend(ax; position = :lt, framevisible = true)
-    Legend(
-        fig[0, 1],
-        ax;
-        tellwidth = false,
-        orientation = :horizontal,
-        # nbanks = 2,
-        framevisible = false,
-    )
-    rowgap!(fig.layout, 5)
+    fig = Figure(; size = (410, 750))
+    for (i, experiment) in enumerate(["volavg", "project_volavg", "surfavg"])
+        islast = i == 3
+        relerr = load(joinpath(datadir, "$(experiment)_relerr.jld2"), "relerr")
+        ax = Axis(
+            fig[i, 1];
+            xlabel = "Time",
+            ylabel = "Relative error",
+            xticksvisible = islast,
+            xticklabelsvisible = islast,
+            xlabelvisible = islast,
+        )
+        lines!(ax, relerr.time, relerr.nomodel; label = "No-model")
+        lines!(ax, relerr.time, relerr.classic; label = "Classic")
+        lines!(ax, relerr.time, relerr.swapfil_symm; label = "Swap-sym")
+        lines!(ax, relerr.time, relerr.swapfil; label = "Swap")
+        # axislegend(ax; position = :lt, framevisible = true)
+        i == 1 && Legend(
+            fig[0, 1],
+            ax;
+            tellwidth = false,
+            orientation = :horizontal,
+            # nbanks = 2,
+            framevisible = false,
+        )
+        Label(
+            fig[i, 1],
+            Dict("volavg" => "VA", "project_volavg" => "PVA", "surfavg" => "SA")[experiment];
+            # Dict(
+            #     "volavg" => "Volume-average",
+            #     "project_volavg" => "Projected volume-average",
+            #     "surfavg" => "Surface-average",
+            # )[experiment];
+            # fontsize = 26,
+            font = :bold,
+            padding = (10, 10, 10, 10),
+            halign = :left,
+            valign = :top,
+            tellwidth = false,
+            tellheight = false,
+        )
+    end
+    # rowgap!(fig.layout, 5)
+    # rowgap!(fig.layout, 10)
     # ylims!(ax, -0.03, 0.34)
-    file = joinpath(plotdir, "$(experiment)_ns-error.pdf")
+    file = joinpath(plotdir, "ns-error.pdf")
     @info "Saving error plot to $file"
     flush(stderr)
-    save(file, fig; backend = CairoMakie, size = (400, 330))
+    save(file, fig; backend = CairoMakie)
     fig
 end
 
 # Plot spectrum
 let
-    specs, D = load(joinpath(datadir, "$(experiment)_spectra.jld2"), "specs", "D")
-    xslope = specs.dns_ref.k[8:(end-12)]
-    yslope = @. 1.58 * D^(2 / 3) * (2π * xslope)^(-5 / 3)
-    specs = (; specs..., kolmogo = (; k = xslope, s = yslope))
-    fig = Figure()
-    ax_full = Makie.Axis(fig[1, 1]; xscale = log2, yscale = log10, xlabel = "Wavenumber")
-    o = 10
-    ax_zoom = zoombox!(
-        fig[1, 1],
-        ax_full;
-        point = (specs.dns_fil.k[end-o], specs.dns_fil.s[end-o]),
-        logx = 1.2,
-        logy = 1.8,
-        relwidth = 0.45,
-        relheight = 0.55,
-    )
-    for (key, label, color, linestyle) in [
-        (:nomodel, "No-model", Cycled(2), :solid),
-        (:classic, "Classic", Cycled(3), :solid),
-        (:swapfil, "Swap", Cycled(4), :solid),
-        (:swapfil_symm, "Swap-sym", Cycled(5), :solid),
-        (:dns_ref, "DNS", Cycled(1), :solid),
-        (:dns_fil, "Filtered DNS", Cycled(1), :dash),
-        (:kolmogo, "Kolmogorov", Cycled(1), :dot),
-    ]
-        spec = specs[key]
-        lines!(ax_full, Point2f.(spec.k, spec.s); color, linestyle, label)
-        lines!(ax_zoom, Point2f.(spec.k, spec.s); color, linestyle)
+    fig = Figure(; size = (400, 750))
+    for (i, experiment) in enumerate(["volavg", "project_volavg", "surfavg"])
+        islast = i == 3
+        specs, D = load(joinpath(datadir, "$(experiment)_spectra.jld2"), "specs", "D")
+        xslope = specs.dns_ref.k[8:(end-12)]
+        yslope = @. 1.58 * D^(2 / 3) * (2π * xslope)^(-5 / 3)
+        specs = (; specs..., kolmogo = (; k = xslope, s = yslope))
+        ax_full =
+            Makie.Axis(fig[i, 1]; xscale = log2, yscale = log10, xlabel = "Wavenumber",
+                ylabel = "Energy",
+                xticksvisible = islast,
+                xticklabelsvisible = islast,
+                xlabelvisible = islast,
+            )
+        o = 10
+        ax_zoom = zoombox!(
+            fig[i, 1],
+            ax_full;
+            point = (specs.dns_fil.k[end-o], specs.dns_fil.s[end-o]),
+            logx = 1.2,
+            logy = 1.8,
+            relwidth = 0.45,
+            relheight = 0.55,
+        )
+        for (key, label, color, linestyle) in [
+            (:nomodel, "No-model", Cycled(2), :solid),
+            (:classic, "Classic", Cycled(3), :solid),
+            (:swapfil, "Swap", Cycled(4), :solid),
+            (:swapfil_symm, "Swap-sym", Cycled(5), :solid),
+            (:dns_ref, "DNS", Cycled(1), :solid),
+            (:dns_fil, "Filtered DNS", Cycled(1), :dash),
+            (:kolmogo, "Kolmogorov", Cycled(1), :dot),
+        ]
+            spec = specs[key]
+            lines!(ax_full, Point2f.(spec.k, spec.s); color, linestyle, label)
+            lines!(ax_zoom, Point2f.(spec.k, spec.s); color, linestyle)
+        end
+        # lines!(ax_full, xslope, yslope; color = Cycled(1), linestyle = :dot, label = "Kolmorov")
+        # lines!(ax_zoom, xslope, yslope; color = Cycled(1), linestyle = :dot)
+        # o = 12
+        # text!(ax_full, xslope[end-o], 1.3 * yslope[end-o]; color = Makie.wong_colors()[1], text = "Kolmorov")
+        # axislegend(ax_full; position = :lb)
+        Label(
+            fig[i, 1],
+            Dict("volavg" => "VA", "project_volavg" => "PVA", "surfavg" => "SA")[experiment];
+            # Dict(
+            #     "volavg" => "Volume-average",
+            #     "project_volavg" => "Projected volume-average",
+            #     "surfavg" => "Surface-average",
+            # )[experiment];
+            # fontsize = 26,
+            font = :bold,
+            padding = (10, 10, 10, 10),
+            halign = :right,
+            valign = :top,
+            tellwidth = false,
+            tellheight = false,
+        )
+        i == 1 && Legend(
+            fig[0, 1],
+            ax_full;
+            tellwidth = false,
+            orientation = :horizontal,
+            nbanks = 3,
+            framevisible = false,
+        )
     end
-    # lines!(ax_full, xslope, yslope; color = Cycled(1), linestyle = :dot, label = "Kolmorov")
-    # lines!(ax_zoom, xslope, yslope; color = Cycled(1), linestyle = :dot)
-    # o = 12
-    # text!(ax_full, xslope[end-o], 1.3 * yslope[end-o]; color = Makie.wong_colors()[1], text = "Kolmorov")
-    # axislegend(ax_full; position = :lb)
-    Legend(
-        fig[0, 1],
-        ax_full;
-        tellwidth = false,
-        orientation = :horizontal,
-        nbanks = 3,
-        framevisible = false,
-    )
     # vlines!(ax_full, [g_dns.n / 2, g_les.n / 2])
     # vlines!(ax_full, 150)
     # ylims!(ax_full, 1e-4, 1e-1)
-    rowgap!(fig.layout, 5)
-    file = joinpath(plotdir, "$(experiment)_ns-spectra.pdf")
+    # rowgap!(fig.layout, 5)
+    file = joinpath(plotdir, "ns-spectra.pdf")
     @info "Saving spectrum plot to $file"
     flush(stderr)
-    save(file, fig; backend = CairoMakie, size = (400, 380))
+    save(file, fig; backend = CairoMakie)
     fig
 end
